0191bcf512e5ff085988285928b16a6900f988c1
Bruno Lovric <brunoolovric@gmail.com>
2021-03-19 18:43:49 +0100

    added multi-dimensional array project

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -8,5 +8,17 @@ var temps = [
 function myArrayFunction(arr) {
     var newTemps = [...arr];
     var averageDayTemp = [];
-    
+    var average = 0;
+    for (var i = 0; i < newTemps.length; i++) {
+        var sum = 0;
+        for (var j = 0; j < newTemps[i].length; j++) {
+            sum += newTemps[i][j];
+        }
+        average = sum / newTemps[i].length;
+        averageDayTemp.push(average);
+    }
+    return averageDayTemp;
 }
+console.log(myArrayFunction(temps));
+module.exports = myArrayFunction;
+    
