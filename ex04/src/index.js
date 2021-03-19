2757c9321d3cd25a6e193c678c7a5372fe9c550c
Bruno Lovric <brunoolovric@gmail.com>
2021-03-19 10:42:36 +0100

    arrays

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    var monitorsList = [];
+    i = 0;
+    for (i = 0; i < newMonitorsList.length; i++) {
+        
+    }
+    
+}
